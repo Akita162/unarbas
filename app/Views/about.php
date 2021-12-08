8836e8d5d4d295dbedaab57ba4a0832f40f6fe2e
zaynox <zayno4d@gmail.com>
2021-12-08 12:14:09 +0700

    menambah about

---
+++ app/Views/about.php
@@ -0,0 +1,33 @@
+<?= $this->extend('layout/template') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container py-5 px-2" id="aboutt">
+    <div class="mb-4">
+        <h2 class="text-center">Sejarah Unarbas</h2>
+        <p>Universitas Unarbas adalah kampus modern, multi budaya, kreatif, dan juga bebas yang mengcakup 
+            kebebasan dalam berekspresi. Unarbas saat ini berusaha untuk menjadi kampus dengan penuh 
+            kebebasan agar mahasiswa bisa menjadi lebih kreatif, independen dan juga tidak tertekan dengan 
+            hal tersebut kami berharap mahasiswa akan mendapatkan ilmu dengan cara yang lebih baik dan juga 
+            efektif. Walaupun sebagai universitas yang bebas, kami tetap menjunjung kedisiplinan dan norma 
+            norma yang ada, agar sistem yang kami gunakan tetap dalam kontrol. Unarbas juga berkomitmen 
+            untuk membuat Indonesia menjadi lebih baik melalui pengembangan kreatifan dan aktivitas 
+            penelitian dari sistem yang ada di Unarbas
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non repudiandae ratione eaque consectetur 
+            illo quos in laborum accusamus inventore ad impedit laudantium eveniet tempora, magnam sapiente 
+            odio repellendus vero iure. Doloribus enim a facilis nesciunt expedita vero minus, ab, ex sapiente 
+            cumque, quae veritatis natus exercitationem. In quos illum qui?
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum accusamus autem ad, eum laborum 
+            porro a numquam quam deleniti est dolorum quod fuga magni aliquam voluptas quae totam nesciunt 
+            velit non atque labore veritatis, corporis dicta facere! Molestiae repellendus corporis tempore 
+            ipsa consectetur sequi aliquam quas quia tenetur omnis eligendi fugit voluptatum enim qui 
+            accusamus, cumque quod sunt! Distinctio culpa dolore cupiditate repellat amet veniam suscipit 
+            officiis! Facere, reprehenderit nihil suscipit quam explicabo maiores dolore ab consequuntur 
+            laborum, numquam sit saepe quisquam quos expedita atque, corporis velit praesentium ea distinctio 
+            impedit perferendis! In perspiciatis voluptate blanditiis sunt officia recusandae velit!
+        </p>
+    </div>
+</div>
+<?= $this->endSection() ?>
